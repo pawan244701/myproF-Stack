09065595d7866d4581fe2f8d4689b1bed0398a42
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-06-26 15:19:16 +0530

    writed down tis

---
+++ frontend/login.js
@@ -0,0 +1,23 @@
+document.getElementById('loginFrm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const uniqeName = document.getElementById('uniqeName').value;
+    const pass = document.getElementById('loginPass').value;
+
+    const response = await fetch('/api/auth/login', {
+        method: "POST",
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            uniqeName, pass
+        })
+    });
+    if (response.ok) {
+        const data = await response.json();
+        window.location.href = 'home.html'
+    } else {
+        const errdata = await response.json();
+        alert(errdata.message || 'wrong password or uniqe name');
+    }
+});
