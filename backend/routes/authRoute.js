b7b252e22012ed7c837e2f95db9e00e5177b70eb
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-06-26 15:15:04 +0530

    add login route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const authControllerReg = require('../controllers/authControllerReg');
+const authControllerLogin = require('../controllers/authControllerLogin');
 
 // email otp varification
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOTP);
 // registration
 router.post('/reg', authControllerReg.regUser);
+// login
+router.post('/login', authControllerLogin.loginUser)
 
 module.exports= router;
