3b17be608cc5eef02cb5143c502611ec3765fe30
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-06-26 11:38:34 +0530

    added reg route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authControllerReg = require('../controllers/authControllerReg');
 
+// email otp varification
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOTP);
+// registration
+router.post('/reg', authControllerReg.regUser);
 
 module.exports= router;
