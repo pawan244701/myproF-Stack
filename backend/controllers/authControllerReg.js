ad8e26044b477310a1a6f6d0ad96420edb746d45
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-06-27 09:42:26 +0530

    added condition to chexk given username is uniwe or not

--- backend/controllers/authControllerReg.js
+++ backend/controllers/authControllerReg.js
@@ -2,15 +2,24 @@ require('dotenv').config();
 const db = require('../config/database');
 
 exports.regUser = async (req, res) => {
-    const {realName, gender, contory, state, district, uniqeName, password, confirmPassword} = req.body;
-    try{
+    const { realName, gender, contory, state, district, uniqeName, password, confirmPassword } = req.body;
+    try {
+        const [firstRows] = await db.query(
+            "SELECT * FROM registration WHERE uniqeName = ?", [uniqeName]
+        );
+        if (firstRows.length > 0) {
+            return res.status(400).json({
+                message: "user allready exist! try with diffrent uniqe name 400"
+            });
+        }
         const [rows] = await db.query(
-"INSERT INTO registration (realName, gender, contory, state, district, uniqeName, password, confirmPassword) VALUES (?,?,?,?,?,?,?,?)", 
-[realName, gender, contory, state, district, uniqeName, password, confirmPassword]
+            "INSERT INTO registration (realName, gender, contory, state, district, uniqeName, password, confirmPassword) VALUES (?,?,?,?,?,?,?,?)",
+            [realName, gender, contory, state, district, uniqeName, password, confirmPassword]
         );
         return res.status(200).json({
             message: "reg successfull."
         });
+
     } catch (err) {
         return res.status(500).json({
             message: "reg failed! try again please"
