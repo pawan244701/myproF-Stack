a668ed79e35453a6ff7449e8c12182daaea21d74
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-05 06:14:52 +0530

    hashing pasword using bcrypt to authControllerReg.js, adding password matching condition to authControllerLogin.js

--- backend/controllers/authControllerLogin.js
+++ backend/controllers/authControllerLogin.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const db = require('../config/database');
+const bcrypt = require('bcrypt');
 
 exports.loginUser = async (req, res) => {
     const { uniqeName, pass } = req.body; // obj destructuring: took 30 min+ to debug. juct typed var incorretliy
@@ -10,8 +11,10 @@ exports.loginUser = async (req, res) => {
                 message: "no user or wrong password 400"
             });
         }
-        const user = rows[0];
-        if (pass !== user.password) {
+
+        const user = rows[0]; // getting the user obj
+        const isPasswordMached = await bcrypt.compare(pass, user.password); // bcrypt password comparision
+        if (isPasswordMached !== user.password) {
             return res.status(401).json({
                 message: "no user or wrong password 401"
             });
