7d297a4bf886398e788da497f631e447e6e4a9d6
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-10 16:22:37 +0530

    adding social media links

--- frontend/home.js
+++ frontend/home.js
@@ -1,3 +1,4 @@
+// logout button
 const logoutbtn = document.getElementById('logoutBtn');
 logoutbtn.addEventListener('click', () => {
     sessionStorage.clear();
@@ -5,15 +6,26 @@ logoutbtn.addEventListener('click', () => {
     window.location.href = 'login.html'
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const savedName = sessionStorage.getItem('loggedInUser');
-    const greetH1 = document.getElementById('welcome-user');
-    const sidebarUserName = document.getElementById('user-profile-name');
+// user login acceptance or denial
+const savedName = sessionStorage.getItem('loggedInUser');
+const greetH1 = document.getElementById('welcome-user');
+const sidebarUserName = document.getElementById('user-profile-name');
+if (savedName) {
+    greetH1.textContent = `Welcome ${savedName}!`;
+    sidebarUserName.textContent = `${savedName}`;
+} else {
+    window.location.href = 'login.html';
+}
 
-    if (savedName) {
-        greetH1.textContent = `Welcome ${savedName}!`;
-        sidebarUserName.textContent = `${savedName}`;
-    } else {
-        window.location.href = 'login.html';
-    }
-});
+// link showing and hiding on click on pawan244701 text link
+const pawan244701 = document.getElementById('pawan244701');
+const links = document.getElementById('social-media-links');
+if (pawan244701 && links) {
+    pawan244701.addEventListener('click', (e) => {
+        e.preventDefault();
+        links.classList.toggle('show');
+    });
+}
+
+
+
